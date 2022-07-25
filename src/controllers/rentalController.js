068921901b391bfda2ee736326053efbdabee41d
Eduardo Pacheco <eduardooliveirapacheco131@gmail.com>
2022-07-24 22:19:28 -0300

    Listar aluguéis concluído

--- src/controllers/rentalController.js
+++ src/controllers/rentalController.js
@@ -1,6 +1,50 @@
 import connection from "../dbStrategy/postgres.js";
 import dayjs from "dayjs";
 
+export async function getRentals(req, res) {
+    let { gameId, customerId} = req.query;
+    let query = '';
+
+    if(gameId) {
+        query = `WHERE rentals."gameId" = ${gameId}`;
+    }
+    if(customerId) {
+        query = `WHERE rentals."customerId" = ${customerId}`;
+    }
+    if( gameId && customerId ) {
+        query = `WHERE rentals."gameId" = ${gameId} and rentals."customerId" = ${customerId}`;
+    }
+    console.log(query);
+    try {
+        const { rows: rentals } = await connection.query(`
+            SELECT 
+                rentals.*,
+                jsonb_build_object(
+                    'id', customers.id,
+                    'name', customers.name
+                ) as customer,
+                jsonb_build_object(
+                    'id', games.id,
+                    'name', games.name,
+                    'categoryId', categories.id,
+                    'categoryName', categories.name
+                ) as game
+            FROM rentals
+            JOIN customers
+            ON rentals."customerId" = customers.id
+            JOIN games
+            ON rentals."gameId" = games.id
+            JOIN categories
+            ON games."categoryId" = categories.id
+            ${query}
+        `);
+        res.send(rentals);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send(error);
+    }
+}
+
 export async function postRental(req, res) {
     const rental = req.body;
     try {
--- src/routers/getRouters.js
+++ src/routers/getRouters.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { getCategories } from "../controllers/categoryController.js";
 import { getCustomer, getCustomers } from "../controllers/customer.Controller.js";
 import { getGames } from "../controllers/gameController.js";
+import { getRentals } from "../controllers/rentalController.js";
 
 const router = Router();
 
@@ -9,5 +10,6 @@ router.get("/categories", getCategories);
 router.get("/games", getGames);
 router.get("/customers", getCustomers);
 router.get("/customers/:id", getCustomer);
+router.get("/rentals", getRentals);
 
 export default router;
